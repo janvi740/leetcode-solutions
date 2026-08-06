f3c6b06ed7c36df4e6e14df379364383a9002602
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-08-06 22:47:50 +0530

    Time: 0 ms (100%), Space: 45.2 MB (9.99%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -2,19 +2,27 @@ class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
 
-        int prev1 = cost[1];
-        int prev2 = cost[0];
+        int[] dp = new int[n];
 
-        for(int index=2; index<n; index++){
-            int oneStep = cost[index] + prev1;
-            int twoStep = cost[index] + prev2;
-
-            int curr = Math.min(oneStep, twoStep);
-
-            prev2 = prev1;
-            prev1 = curr;
+        if(n==1){
+            return cost[0];
         }
 
-        return Math.min(prev1, prev2);
+        if(n==2){
+            return Math.min(cost[0], cost[1]);
+        }
+        
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int index=2; index<n; index++){
+
+            int oneStep = cost[index] + dp[index-1];
+            int twoStep = cost[index] + dp[index-2];
+
+            dp[index] = Math.min(oneStep, twoStep);
+        }
+
+        return Math.min(dp[n-2], dp[n-1]);
     }
 }
