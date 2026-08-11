1c7d23912d40dbab0f3a88f25c2bdbeffed5e634
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-08-11 21:20:56 +0530

    Time: 30 ms (29.24%), Space: 45.8 MB (97.12%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,67 +1,57 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
+        
         int rows = grid.length;
         int cols = grid[0].length;
 
-        int[][] front = new int[cols][cols];
+        int[][][] dp = new int[rows][cols][cols];
 
         for(int j1=0; j1<cols; j1++){
             for(int j2=0; j2<cols; j2++){
 
-                if(j1==j2){
-                    front[j1][j2] = grid[rows-1][j1];
+                if(j1 == j2){
+                    dp[rows-1][j1][j2] = grid[rows-1][j1];
                 }
                 else{
-                    front[j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
+                    dp[rows-1][j1][j2] = grid[rows-1][j1] + grid[rows-1][j2];
                 }
             }
         }
 
         for(int i=rows-2; i>=0; i--){
-
-            int[][] curr = new int[cols][cols];
-
             for(int j1=0; j1<cols; j1++){
                 for(int j2=0; j2<cols; j2++){
-                    
+
                     int currentCherries;
 
-                    if(j1==j2){
+                    if(j1 == j2){
                         currentCherries = grid[i][j1];
-                    }
-                    else{
+                    }else{
                         currentCherries = grid[i][j1] + grid[i][j2];
                     }
 
-                    int maxCherries = Integer.MIN_VALUE;
-
+                    int max = Integer.MIN_VALUE;
+                    
                     for(int dj1=-1; dj1<=1; dj1++){
                         for(int dj2=-1; dj2<=1; dj2++){
 
-                            int nextj1 = j1 + dj1;
-                            int nextj2 = j2 + dj2;
+                            int newJ1 = j1 + dj1;
+                            int newJ2 = j2 + dj2;
 
-                            int next;
+                            if(newJ1>=0 && newJ1<cols && newJ2>=0 && newJ2<cols){
 
-                            if(nextj1 >= 0 && nextj1 < cols && nextj2 >= 0 && nextj2 < cols){
+                                int next = dp[i+1][newJ1][newJ2];
 
-                                next = front[nextj1][nextj2];
+                                max = Math.max(max, currentCherries + next);
                             }
-                            else{
-                                next = -1_000_000_000;
-                            }
-
-                            maxCherries = Math.max(maxCherries, currentCherries + next);
                         }
                     }
 
-                    curr[j1][j2] = maxCherries;
+                    dp[i][j1][j2] = max;   
                 }
             }
-
-            front = curr;
         }
 
-        return front[0][cols-1];
+        return dp[0][0][cols-1];
     }
 }
