49e40b4c2cf34f235b49e02815b47c994d622177
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-08-16 00:07:21 +0530

    Time: 5 ms (80.73%), Space: 44.9 MB (50.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,56 +1,50 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-
         int totalSum = 0;
-        for (int num : nums) {
+
+        for(int num : nums){
             totalSum += num;
         }
 
-        if (Math.abs(target) > totalSum) {
+        if(Math.abs(target) > totalSum){
             return 0;
         }
-        
+
         if((totalSum + target) % 2 != 0){
             return 0;
         }
-        
-        int subsetTarget = (totalSum + target) / 2;
-        
-        int[] prev = new int[subsetTarget+1];
 
-        if(nums[0]==0){
-            prev[0] = 2;
+        int subsetTarget = (totalSum + target) / 2;
+
+        int[][] dp = new int[n][subsetTarget+1];
+        
+        if(nums[0] == 0){
+            dp[0][0] = 2;
         }
         else{
-            prev[0] = 1;
-        }
+            dp[0][0] = 1;
 
-        if(nums[0]!=0 && nums[0] <= subsetTarget){
-            prev[nums[0]] = 1;
+            if(nums[0] <= subsetTarget){
+                dp[0][nums[0]] = 1;
+            }
         }
 
         for(int index=1; index<n; index++){
-
-            int[] curr = new int[subsetTarget+1];
-            curr[0] = 0;
-
             for(int sum=0; sum<=subsetTarget; sum++){
 
-                int notPick = prev[sum];
+                int notPick = dp[index-1][sum];
 
                 int pick = 0;
 
                 if(nums[index] <= sum){
-                    pick = prev[sum-nums[index]];
+                    pick = dp[index-1][sum-nums[index]];
                 }
 
-                curr[sum] = pick + notPick;
+                dp[index][sum] = pick + notPick;
             }
-
-            prev = curr;
         }
 
-        return prev[subsetTarget];
+        return dp[n-1][subsetTarget];
     }
 }
