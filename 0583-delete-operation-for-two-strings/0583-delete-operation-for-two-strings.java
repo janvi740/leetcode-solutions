645cf615b10957bc61a67d785681b0576bccf791
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-08-26 21:05:48 +0530

    Time: 6 ms (95.17%), Space: 46.4 MB (50.17%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -8,27 +8,24 @@ class Solution {
         return (m+n) - 2*lcs;
     }
 
-    public int solve(String s1, String s2){
-        int n = s1.length();
-        int m = s2.length();
+    public int solve(String word1, String word2){
+        int n = word1.length();
+        int m = word2.length();
 
-        int[] prev = new int[m+1];
+        int[][] dp = new int[n+1][m+1];
 
         for(int i=1; i<=n; i++){
-            int[] curr = new int[m+1];
-
             for(int j=1; j<=m; j++){
 
-                if(s1.charAt(i-1) == s2.charAt(j-1)){
-                    curr[j] = 1 + prev[j-1];
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
                 else{
-                    curr[j] = Math.max(curr[j-1], prev[j]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
-            prev = curr;
         }
 
-        return prev[m];
+        return dp[n][m];
     }
 }
