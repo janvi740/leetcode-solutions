8c1b147dc542fd0d488e0bebb459f5e404348f9d
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-09-02 22:55:54 +0530

    Time: 8 ms (89%), Space: 48.5 MB (60.05%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0; i<2*n; i++){
+
+            int index = i % n;
+            int current = nums[index];
+
+            while(!stack.isEmpty() && current > nums[stack.peek()]){
+                int prevIndex = stack.pop();
+                ans[prevIndex] = current;
+            }
+
+            if(i < n){
+                stack.push(index);
+            }
+        }
+
+        return ans;
+    }
+}
