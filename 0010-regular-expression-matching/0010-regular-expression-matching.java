6f36550a51e9910837b76fffb55e74334ad7743f
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-09-04 22:40:54 +0530

    Time: 2 ms (46.83%), Space: 43.6 MB (33.44%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -3,35 +3,39 @@ class Solution {
         int n = s.length();
         int m = p.length();
 
-        boolean[][] dp = new boolean[n+1][m+1];
+        boolean[] prev = new boolean[m+1];
 
-        dp[0][0] = true;
+        prev[0] = true;
 
         for(int j=2; j<=m; j++){
             if(p.charAt(j-1) == '*'){
-                dp[0][j] = dp[0][j-2];
+                prev[j] = prev[j-2];
             }
         }
 
         for(int i=1; i<=n; i++){
+            boolean[] curr = new boolean[m+1];
+
             for(int j=1; j<=m; j++){
 
                 if(p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '.'){
-                    dp[i][j] = dp[i-1][j-1];
+                    curr[j] = prev[j-1];
                 }
 
                 else if(p.charAt(j-1) == '*'){
                     //0 occurence
-                    dp[i][j] = dp[i][j-2];
+                    curr[j] = curr[j-2];
 
                     // one or more occurence
                     if(p.charAt(j-2) == s.charAt(i-1) || p.charAt(j-2) == '.'){
-                        dp[i][j] = dp[i][j] || dp[i-1][j];
+                        curr[j] = curr[j] || prev[j];
                     }
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 }
