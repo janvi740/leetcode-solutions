18531b438a66639172ea71a1be36759c71f878d4
Janvi Mittal <70996677+janvi740@users.noreply.github.com>
2026-09-24 02:06:46 +0530

    Time: 1 ms (99.47%), Space: 45.1 MB (64.6%) - LeetHub

---
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    static class Pair{
+        TreeNode node;
+        long index;
+
+        Pair(TreeNode node, long index){
+            this.node = node;
+            this.index = index;
+        }
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0));
+
+        int maxWidth = 0;
+
+        while(!queue.isEmpty()){
+
+            int size = queue.size();
+
+            long minIndex  = queue.peek().index;
+
+            long first = 0;
+            long last = 0;
+
+            for(int i=0; i<size; i++){
+
+                Pair pair = queue.poll();
+
+                TreeNode node = pair.node;
+                long index = pair.index - minIndex;
+
+                if (i == 0) {
+                    first = index;
+                }
+
+                if (i == size - 1) {
+                    last = index;
+                }
+
+                if(node.left != null){
+                    queue.offer(new Pair(node.left, 2*index+1));
+                }
+
+                if(node.right != null){
+                    queue.offer(new Pair(node.right, 2*index+2));
+                }
+            }
+
+            maxWidth = Math.max(maxWidth, (int)(last-first+1));
+        }
+
+        return maxWidth;
+    }
+}
